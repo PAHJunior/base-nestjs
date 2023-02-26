98332df13e1234cde6c2e8acf25d42964a906897
Paulo Junior <paulo.junior@gorilainvest.com.br>
2023-02-26 11:42:08 -0300

    adding script to prepare commit msg

---
+++ scripts/prepare-commit-msg.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This way you can customize which branches should be skipped when
+# prepending commit message.
+if [ -z "$BRANCHES_TO_SKIP" ]; then
+  BRANCHES_TO_SKIP=(develop master)
+fi
+
+BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+
+BRANCH_EXCLUDED=$(printf "%s\n" "${BRANCHES_TO_SKIP[@]}" | grep -c "^$BRANCH_NAME$")
+BRANCH_IN_COMMIT=$(grep -c "\[$BRANCH_NAME\]" $1)
+
+if [ -n "$BRANCH_NAME" ] && ! [[ $BRANCH_EXCLUDED -eq 1 ]] && ! [[ $BRANCH_IN_COMMIT -ge 1 ]]; then
+  msg=$(cat $HUSKY_GIT_PARAMS)
+  echo "[$BRANCH_NAME] - $msg" > "$1"
+fi
